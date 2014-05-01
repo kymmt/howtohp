3870e84b1aa5ee4e3b129c2d2be5acfc80cb9b8f
kymmt <contact@koheiyamamoto.me>
2014-05-02 04:29:10 +0900

    fix jekyll:check option

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,7 +218,7 @@ module.exports = function (grunt) {
             },
             check: {
                 options: {
-                    doctor: false
+                    doctor: false,
                     dest: '.jekyll'
                 }
             }
